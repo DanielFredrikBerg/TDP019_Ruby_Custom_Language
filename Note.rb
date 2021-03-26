0ff5d03dbdfd1c066f540c24c0597e76cc315426
theodore nilsson <theni230@student.liu.se>
2021-03-26 11:56:04 +0100

    made octave and length keyword arguments

--- Note.rb
+++ Note.rb
@@ -1,8 +1,17 @@
 class Note
 
-  def initialize(tone, length = 1, octave = 0)
+  def initialize(tone, length: 1, octave: 0)
     @tone = tone
-    @length = 1/length
+    @length = Rational(1,length)
     @octave = 0 + octave
-  end  
+  end
+
+  def print
+    puts @length.denominator.to_s + @tone.to_s + @octave.to_s
+  end
+  
 end
+
+note = Note.new('b', octave: -1)
+
+note.print
