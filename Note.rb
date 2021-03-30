31a8b7085fee56096fdce5d378aa790a2fd05529
danhu849 <danhu849@gitlab.liu.se>
2021-03-30 16:52:33 +0200

    added shell script for automatic Note.rb pull from other branch

--- Note.rb
+++ Note.rb
@@ -14,34 +14,51 @@ class Note
   end
 
   def octave(halfstep = @halfstep)
+    #Returns which octave a tone(halfstep) is in
     halfstep / 12
   end
 
   def tone(halfstep = @halfstep)
+    #returns the name of a tone(halfstep)
     @scale[halfstep % 12]
   end
   
   def octave_to_halfstep(octave)
+    #returns the "halfstep"-value of c in a given octave
     12 * octave
   end
   
   def tone_to_halfstep(tone)
-    t2h = {'c' => 0,
-           'c#' => 1,
-           'd' => 2,
-           'd#' => 3,
-           'e' => 4,
-           'f' => 5,
-           'f#' => 6,
-           'g' => 7,
-           'g#' => 8,
-           'a' => 9,
-           'a#' => 10,
-           'b' => 11}
+    #given a tone expressed in symbols, will return a halfstep-value for that tone in the middle octave
+    t2h = {
+      'cb'=>-1,
+      'c'=>0,
+      'c#'=>1,
+      'db'=>1,
+      'd'=>2,
+      'd#'=>3,
+      'eb'=>3,
+      'e'=>4,
+      'e#'=>5,
+      'fb'=>4,
+      'f'=>5,
+      'f#'=>6,
+      'gb'=>6,
+      'g'=>7,
+      'g#'=>8,
+      'ab'=>8,
+      'a'=>9,
+      'a#'=>10,
+      'bb'=>10,
+      'b'=>11,
+      'b#'=>12
+    }
+
     t2h[tone]    
   end
 
   def semitone_to_halfstep(semitone='')
+    # #=>+1, b=> -1. 
     case semitone
     when '#'
       +1
@@ -68,30 +85,30 @@ class Note
   end
  
   def transposed(halfsteps = 1)
-    #Will return a note transposed the given number of steps.
+    #Will return a note transposed the given number of halfsteps.
       Note.new(@length.denominator, self.tone(@halfstep + halfsteps), self.octave(@halfstep + halfsteps) )
   end
   
 end
 
-# note = Note.new(2,'c#',-1)
-# note2 = Note.new(1,'b', 0)
-# note3 = Note.new(2,'b',+1)
-# note4 = Note.new(1,'g',-2)
+note = Note.new(2,'b#',-1)
+note2 = Note.new(1,'b', 0)
+note3 = Note.new(2,'b',+1)
+note4 = Note.new(1,'g',-2)
 
-# note.write
-# note2.write
-# note3.write
-# note3.transposed.write
-# note4.write
-# note4.transposed.write
-# note4.transposed(2).write
+note.write
+note2.write
+note3.write
+note3.transposed.write
+note4.write
+note4.transposed.write
+note4.transposed(2).write
 
-# puts ""
+puts ""
 
-# puts note.halfstep
-# puts note2.halfstep
-# puts note3.halfstep
-# puts note4.halfstep
+puts note.halfstep
+puts note2.halfstep
+puts note3.halfstep
+puts note4.halfstep
 
-# puts ""
+puts ""
---
+++ update_note.sh
@@ -0,0 +1,6 @@
+git checkout Note_Class
+git pull
+sleep 5s
+git checkout parser_and_tests
+git checkout Note_Class -- Note.rb
+emacs Note.rb&
