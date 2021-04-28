e18c944ed24e1f79841556034a5ce3f0d9d4a789
danhu849 <danhu849@gitlab.liu.se>
2021-04-28 11:34:20 +0200

    changed examplecode

--- Repeat.rb
+++ Repeat.rb
@@ -1,16 +1,20 @@
 class Repeat
 
-  def initialize(iterations, loop_list) 
+  def initialize(iterations, loop_list, scope) 
     @iterations = iterations.seval
     @loop_list = loop_list
   end
   
   def seval
+    @@Stackframe.add(stack)
+
     (1..@iterations).each do
       @loop_list.each do |statement|
         statement.seval
       end
     end
+
+    @@Stackfram.pop
   end
   
 end
